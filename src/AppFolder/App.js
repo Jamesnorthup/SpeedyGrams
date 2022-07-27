2af32d82dd87e7873d4b774b45571b3d75acf92b
deon <mkandaniel@gmail.com>
2022-07-27 10:41:30 -0700

    organizing app fil structure

--- src/AppFolder/App.js
+++ src/AppFolder/App.js
@@ -1,53 +1,35 @@
-
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Post from '../Components/Posts/Post';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import { Route, Redirect } from "react-router-dom";
+import HomePage from "../Components/HomePage";
+import Profile from "../Components/Profile";
 
 function App() {
-
   const [posts, setPosts] = useState([
-  {
-    username: "James",
-    caption: "  Wow it works!",
-    imageUrl: "https://patterns.dev/img/reactjs/react-logo@3x.svg/"
-  },
-  {
-    username: "James",
-    caption: "  Wow it works!",
-    imageUrl: "https://patterns.dev/img/reactjs/react-logo@3x.svg/"
-  }
-
-]);
-
+    {
+      username: "James",
+      caption: "  Wow it works!",
+      imageUrl: "https://patterns.dev/img/reactjs/react-logo@3x.svg/",
+    },
+    {
+      username: "James",
+      caption: "  Wow it works!",
+      imageUrl: "https://patterns.dev/img/reactjs/react-logo@3x.svg/",
+    },
+  ]);
 
   return (
     <div className="app">
-
-
-      {/* Header */}
-      <div className="app_header">
-        <img
-          className="app_headerImage"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="" />
-
-
-      </div>
-
-
-      <h1>Hello World</h1>
-
-      {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        ))
-      }
-    
-      {/* Posts */}
+      <header>
+        {/* Insert navbar componenet here */}
+      </header>
+      <Container>
+        <Route path="/home" render={() => <HomePage />} />
+        <Route path="/profile" render={() => <Profile />} />
+      </Container>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/Container"
+
+const HomePage = () => {
+    return (
+        <Container>
+            <div>
+                <h1>Hello</h1>
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage
