a98b57eeac85c58896903d96645af31f893acc12
deon <mkandaniel@gmail.com>
2022-07-28 20:49:09 -0700

    completed profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,13 +1,48 @@
-import Container from 'react-bootstrap/Container'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Post from "./Posts/Post";
 
 const Profile = () => {
-    return (
-        <Container>
-            <div>
-                <h1>Profile</h1>
-            </div>
-        </Container>
-    )
-}
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const [myPosts, setMyPosts] = useState();
 
-export default Profile
+  const makeApiCall = async () => {
+    const res = await fetch("http://localhost:4000/posts/myposts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: user.email,
+      }),
+    });
+    const json = await res.json();
+    setMyPosts(json.posts);
+  };
+  makeApiCall();
+
+  return (
+    <Container>
+      <h2>My Posts</h2>
+      {myPosts &&
+        myPosts.map((post) => {
+          return (
+            <Row>
+              <Post
+                user={user}
+                creator={post.creator}
+                image={post.imageUrl}
+                caption={post.caption}
+                comments={post.comments}
+              />
+            </Row>
+          );
+        })}
+    </Container>
+  );
+};
+
+export default Profile;
